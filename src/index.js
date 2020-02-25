ddd54aa3b46bc08843473c08150d09b77ad7df30
Muthu Kumar <muthukumar@thefeathers.in>
2020-02-25 22:35:36 +0530

    [fix] darwin detected as win
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { platform } from "os";
 import { createFilter } from "rollup-pluginutils";
 import Svelte from "svelte/compiler";
 
-const isWindows = platform().includes("win");
+// Possible values are 'aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', and 'win32'.
+// https://nodejs.org/api/os.html#os_os_platform
+const isWindows = platform() === "win32";
 
 const toSvelte = content => `
 <script>
@@ -34,27 +36,28 @@ const toSvelte = content => `
 const head = xs => xs[0];
 const tail = xs => xs[xs.length - 1];
 
-export default function svg (options = {}) {
+export default function svg(options = {}) {
 	const filter = createFilter(options.include, options.exclude);
 
 	return {
 		name: "svg",
 
-		transform (source, id) {
+		transform(source, id) {
 			if (!filter(id) || extname(id) !== ".svg") {
 				return null;
 			}
 
-			const content = toSvelte(
-				JSON.stringify(source.trim()));
-			const { js: { code, map } } = Svelte.compile(content, {
+			const content = toSvelte(JSON.stringify(source.trim()));
+			const {
+				js: { code, map },
+			} = Svelte.compile(content, {
 				filename: id,
-				name: head(tail(id.split(isWindows? "\\" : "/")).split(".")),
+				name: head(tail(id.split(isWindows ? "\\" : "/")).split(".")),
 				format: "esm",
-				generate: options.generate
+				generate: options.generate,
 			});
 
 			return { code, map };
-		}
-	}
+		},
+	};
 }
