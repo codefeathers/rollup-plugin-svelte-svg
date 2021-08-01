fce28f94e0d5c9d5db30fbae956c37b431e070c2
Muthu Kumar <muthukumar@thefeathers.in>
2021-08-01 05:31:32 +0530

    feat: massive simplification, do not handle compile, pass svg as-is to Svelte plugin
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- src/index.js
+++ src/index.js
@@ -1,75 +1,39 @@
-import { extname } from "path";
-import { platform } from "os";
-import { optimize as optimise } from "svgo";
+const { extname, dirname, resolve } = require("path");
+const { readFile } = require("fs/promises");
 
-import { createFilter } from "rollup-pluginutils";
-import Svelte from "svelte/compiler";
-
-// Possible values are 'aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', and 'win32'.
-// https://nodejs.org/api/os.html#os_os_platform
-const isWindows = platform() === "win32";
-
-const head = xs => xs[0];
-const tail = xs => xs[xs.length - 1];
-
-const validJS = /[a-zA-Z_$][0-9a-zA-Z_$]*/;
-
-const toJSClass = text =>
-	head(tail(text.split(isWindows ? "\\" : "/")).split("."))
-		.split("-")
-		// Uppercase first character of every segment after splitting out hyphens
-		.map(segment => (segment ? segment[0].toUpperCase() + segment.slice(1) : segment))
-		.join("")
-		// split into characters
-		.split("")
-		// drop potentially unsafe characters
-		.map(x => (validJS.test(x) ? x : ""))
-		.join("");
+const { optimize: optimise } = require("svgo");
+const { createFilter } = require("rollup-pluginutils");
 
 const svgRegex = /(<svg.*?)(>.*)/s;
+const svgheader = /^\<\?xml.+?\>/;
 
-export function svelteSVG(options = {}) {
+function addProps(source) {
+	const parts = svgRegex.exec(source);
+	if (!parts) throw new Error("Unable to parse as svg.");
+
+	const [, svgStart, svgBody] = parts;
+	return `${svgStart} {...$$props} ${svgBody}`;
+}
+
+export function svelteSVG({ svgo } = {}) {
 	const filter = createFilter(options.include, options.exclude);
 
 	return {
-		name: "svg",
+		name: "rollup-plugin-svelte-svg",
 
-		transform(source, id) {
-			if (!filter(id) || extname(id) !== ".svg") {
-				return null;
-			}
+		resolveId(source, importer) {
+			if (!filter(source) || extname(source) !== ".svg") return null;
 
-			if (source.startsWith("export default")) {
-				// in sapper we get the SVG compiled as an ESM with
-				// the default export being the SVG encoded as data URI
-				source = source.replace(/(^export default "data:image\/svg\+xml,)|("$)/g, "");
-				source = decodeURIComponent(source);
-			}
+			return resolve(dirname(importer), source + ".svelte");
+		},
 
-			function process(source) {
-				const parts = svgRegex.exec(source);
-				if (!parts) {
-					throw new Error(
-						"svg file did not start with <svg> tag. Unable to convert to Svelte component"
-					);
-				}
-				const [, svgStart, svgBody] = parts;
-				const content = `${svgStart} {...$$props}${svgBody}`;
+		load(id) {
+			if (!id.endsWith(".svg.svelte")) return null;
 
-				const {
-					js: { code, map },
-				} = Svelte.compile(content, {
-					filename: id,
-					name: toJSClass(id),
-					format: "esm",
-					generate: options.generate,
-					hydratable: true,
-					dev: options.dev,
-				});
-				return { code, map };
-			}
-
-			return options.svgo ? process(optimise(source, options.svgo).data) : process(source);
+			return readFile(id.slice(0, -".svelte".length), "utf-8")
+				.then(file => (svgo ? optimise(file, svgo).data : file))
+				.then(file => file.replace(svgheader, "").trim())
+				.then(addProps);
 		},
 	};
 }
